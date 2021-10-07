7877d82b425b630c08a51f06bca5257c396b6366
suphrt <arustamyan_milana@mail.ru>
2021-10-07 10:04:25 +0300

    добавлены файлы

---
+++ code.py
@@ -0,0 +1,10 @@
+print('Enter two numbers, chtoby pervoe bylo bolshe vtorogo')
+x=(int(input()))
+y=(int(input()))
+if x>y:
+    print('Velikolepno! My congratulations! You won a million')
+    print('Do not press enter to get your prize')
+else:
+    print('I had a better opinion of you before')
+    print('Never try again')
+
